c81c8784a3b7ba61b402c822474838b682cfd1d4
Nikhil Gugad <nikhilgugad111@gmail.com>
2025-10-17 09:51:46 +0530

    changed yandex to google

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ const aiBtn = document.querySelector('#aiBtn');
 searchBtn.addEventListener('click', () => {
     const query = searchBox.value.trim();
     if (query) {
-        window.location.href = 'https://yandex.com/search/?text=' + encodeURIComponent(query);
+        window.location.href = 'https://google.com/search/?q=' + encodeURIComponent(query);
     }
 });
 
 searchBox.addEventListener('keypress', (e) => {
     const query = searchBox.value.trim();
     if (e.key == "Enter" && e.ctrlKey == false) {
-        window.location.href = 'https://yandex.com/search/?text=' + encodeURIComponent(query);
+        window.location.href = 'https://google.com/search/?q=' + encodeURIComponent(query);
     }
 });
 
