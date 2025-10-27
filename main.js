dbf4c22733110827e06ce2ee3e53c85a12eb3f63
Nikhil Gugad <nikhilgugad111@gmail.com>
2025-10-27 13:04:54 +0530

    fixing my silly mistake

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ const aiBtn = document.querySelector('#aiBtn');
 searchBtn.addEventListener('click', () => {
     const query = searchBox.value.trim();
     if (query) {
-        window.location.href = 'https://4get.ca/web?s=' + encodeURIComponent(query);
+        window.location.href = 'https://google.com/search?q=' + encodeURIComponent(query);
     }
 });
 
 searchBox.addEventListener('keypress', (e) => {
     const query = searchBox.value.trim();
     if (e.key == "Enter" && e.ctrlKey == false) {
-        window.location.href = 'https://4get.ca/web?s=' + encodeURIComponent(query);
+        window.location.href = 'https://google.com/search?q=' + encodeURIComponent(query);
     }
 });
 
@@ -63,6 +63,7 @@ document.addEventListener('keydown', (e) => {
         appsDropdown.style.height = '400px';
     }
 });
+
 
 
 
